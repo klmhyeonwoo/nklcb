163fca761f7fda864417c78c8dd193d177822135
klmhyeonwoo <gentlemonster77@likelion.org>
2025-05-07 20:56:28 +0900

    fix: safari input focus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
       </head>
       <body>{children}</body>
