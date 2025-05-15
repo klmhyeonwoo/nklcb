b34c69699722e7d5385c865010abf2faaed93af5
klmhyeonwoo <gentlemonster77@likelion.org>
2025-05-15 21:11:44 +0900

    feat: add ad area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,11 @@ export default async function RootLayout({
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1550225145364569"
         crossOrigin="anonymous"
       ></script>
+      <script
+        type="text/javascript"
+        src="//t1.daumcdn.net/kas/static/ba.min.js"
+        async
+      ></script>
       <head />
       <body>{children}</body>
     </html>
--- src/app/recruitment-notices/loading.tsx
+++ src/app/recruitment-notices/loading.tsx
@@ -6,6 +6,13 @@ import Image from "next/image";
 function Loading() {
   return (
     <div className={styles.loading__container}>
+      <ins
+        className="kakao_ad_area"
+        style={{ display: "none" }}
+        data-ad-unit="DAN-sUkFhsulXIMgODgt"
+        data-ad-width="160"
+        data-ad-height="600"
+      ></ins>
       <Image src={icon_clapping} alt="클랩 아이콘" width={80} height={80} />
       <span> 선택하신 공고로 이동을 하고 있어요 </span>
     </div>
